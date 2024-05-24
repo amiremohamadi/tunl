58fbf4a437422ce9c16acc1ef8bcab26598ce828
Amir Mohammadi <amiremohamadi@yahoo.com>
2024-05-24 12:42:50 +0330

    add route for sharing the v2ray link

--- src/config.rs
+++ src/config.rs
@@ -2,10 +2,12 @@ use uuid::Uuid;
 
 pub enum Outbound {
     Vless(VlessConfig),
+    Vmess(VlessConfig),
 }
 
 pub struct VlessConfig {
     pub uuid: Uuid,
+    pub host: String,
 }
 
 pub struct Config {
--- src/lib.rs
+++ src/lib.rs
@@ -5,32 +5,79 @@ mod proxy;
 use crate::config::{Config, Outbound, VlessConfig};
 use crate::proxy::*;
 
+use serde::Serialize;
 use uuid::Uuid;
 use worker::*;
 
 #[event(fetch)]
-async fn main(_: Request, env: Env, _: Context) -> Result<Response> {
+async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
     let uuid = env
         .var("UUID")
         .map(|x| Uuid::parse_str(&x.to_string()).unwrap_or_default())?;
 
+    let outbound = env
+        .var("OUTBOUND")
+        .map(|x| x.to_string())
+        .unwrap_or_default();
+
+    let host = req.url()?.host().map(|x| x.to_string()).unwrap_or_default();
+
     let config = Config {
-        outbound: Outbound::Vless(VlessConfig { uuid }),
+        outbound: match outbound.as_str() {
+            "vmess" => Outbound::Vmess(VlessConfig { uuid, host }),
+            "vless" => Outbound::Vless(VlessConfig { uuid, host }),
+            _ => return Err(Error::RustError("invalid outbound".to_string())),
+        },
     };
 
+    Router::with_data(config)
+        .on_async("/", tunnel)
+        .on("/link", link)
+        .run(req, env)
+        .await
+}
+
+async fn tunnel(_: Request, cx: RouteContext<Config>) -> Result<Response> {
     let WebSocketPair { server, client } = WebSocketPair::new()?;
 
     server.accept()?;
     wasm_bindgen_futures::spawn_local(async move {
         let events = server.events().unwrap();
-        let mut stream = match config.outbound {
-            Outbound::Vless(c) => VmessStream::new(c, &server, events),
-        };
-
-        if let Err(e) = stream.process().await {
+        if let Err(e) = match cx.data.outbound {
+            Outbound::Vless(c) => VlessStream::new(c, &server, events).process().await,
+            Outbound::Vmess(c) => VmessStream::new(c, &server, events).process().await,
+        } {
             console_debug!("error occured during tunneling stream: {}", e);
-        }
+        };
     });
 
     Response::from_websocket(client)
+}
+
+fn link(_: Request, cx: RouteContext<Config>) -> Result<Response> {
+    #[derive(Serialize)]
+    struct Link {
+        description: String,
+        link: String,
+    }
+
+    let link = match cx.data.outbound {
+        Outbound::Vmess(c) => {
+            format!(
+                "vmess://{}@104.24.30.167:80?encryption=zero&type=ws&security=none&host={}#tunl",
+                c.uuid, c.host
+            )
+        }
+        Outbound::Vless(c) => {
+            format!(
+                "vless://{}@104.24.30.167:80?type=ws&security=none&host={}#tunl",
+                c.uuid, c.host
+            )
+        }
+    };
+
+    Response::from_json(&Link {
+        link,
+        description: "visit https://scanner.github1.cloud/ and replace the IP address in the configuration with a clean one".to_string()
+    })
 }
